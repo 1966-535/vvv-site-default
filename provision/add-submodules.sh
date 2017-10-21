7c7b7a4814209f3898c28a9fc8c53d0f289b9bc0
ıqO <merenuou@gmail.com>
2017-10-21 00:07:12 -0600

    Add more setup assets.

--- provision/add-submodules.sh
+++ provision/add-submodules.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-# this file loads all needed plugins as submodules.
+# This file loads all needed plugins as submodules.
+# DO NOT LINK TO AUTO SETUP!!
 
 REPO_GL='git@gitlab.com:'
 REPO_GH='git@github.com:'
 PLUGIN_DIR='../public_html/wp-content/plugins'
+THEME_DIR='../public_html/wp-content/themes'
 
-# read ${PLUGIN_DIR}
 if [ -d ${PLUGIN_DIR} ]; then
     git submodule add -f ${REPO_GL}owllabs/metumaribe-utility.git ${PLUGIN_DIR}/metumaribe-utility
     git submodule add -f ${REPO_GL}qasa/deals-wordpress-plugin.git ${PLUGIN_DIR}/deals-wordpress-plugin
     git submodule add -f ${REPO_GH}Metumaribe/easy-digital-downloads.git ${PLUGIN_DIR}/easy-digital-downloads
     git submodule add -f ${REPO_GH}Metumaribe/WordPress-Admin-Style.git ${PLUGIN_DIR}/WordPress-Admin-Style
     git submodule add -f ${REPO_GH}Metumaribe/timber.git ${PLUGIN_DIR}/timber
+    if [ -d ${THEME_DIR} ]; then
+        git submodule add -f ${REPO_GH}Metumaribe/branch.git ${THEME_DIR}/branch
+        git submodule add -f ${REPO_GH}Metumaribe/timber-bootstrap.git ${THEME_DIR}/timber-bootstrap
+    else
+        echo "${THEME_DIR} doesn't exists"
+    fi
 else
-    echo "That directory doesn't exists"
+    echo "${PLUGIN_DIR} doesn't exists"
 fi
+# DO NOT LINK TO AUTO SETUP!!
